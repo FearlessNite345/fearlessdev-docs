45cd54141d0fdb87da051c4cafdf38abe67b8bc4
FearlessNite345 <110247392+FearlessNite345@users.noreply.github.com>
2025-06-24 05:01:06 -0400

    Added search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,85 +1,96 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'FearlessDev',
-  favicon: 'img/FS-Logo.svg',
+  title: "FearlessDev",
+  favicon: "img/FS-Logo.svg",
 
   // Set the production url of your site here
-  url: 'https://docs.fearlessdev.me',
+  url: "https://docs.fearlessdev.me",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
-          routeBasePath: '/',
-          editUrl: 'https://github.com/FearlessNite345/fearlessdev-docs/tree/main'
+          sidebarPath: "./sidebars.ts",
+          routeBasePath: "/",
+          editUrl:
+            "https://github.com/FearlessNite345/fearlessdev-docs/tree/main",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
   ],
 
+  plugins: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      {
+        hashed: true,
+      },
+    ],
+  ],
+
   themeConfig: {
     colorMode: {
-      defaultMode: 'dark',
+      defaultMode: "dark",
       disableSwitch: true,
-      respectPrefersColorScheme: false
+      respectPrefersColorScheme: false,
     },
     navbar: {
-      title: 'Fearless\'s Docs',
+      title: "Fearless's Docs",
       logo: {
-        alt: 'FearlessStudios Logo',
-        src: 'img/FS-Logo.svg'
+        alt: "FearlessStudios Logo",
+        src: "img/FS-Logo.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'docsSidebar',
-          position: 'left',
-          label: 'Docs',
+          type: "docSidebar",
+          sidebarId: "docsSidebar",
+          position: "left",
+          label: "Docs",
         },
         {
-          href: 'https://store.fearlessdev.me',
-          label: 'Visit our Store',
-          position: 'right',
+          href: "https://store.fearlessdev.me",
+          label: "Visit our Store",
+          position: "right",
         },
         {
-          href: 'https://discord.gg/9FJrnrc7Yp',
-          label: 'Join our Discord',
-          position: 'right',
-        }
+          href: "https://discord.gg/9FJrnrc7Yp",
+          label: "Join our Discord",
+          position: "right",
+        },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       copyright: `Copyright © ${new Date().getFullYear()} FearlessStudios, Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
-      additionalLanguages: ['lua']
+      additionalLanguages: ["lua"],
     },
   } satisfies Preset.ThemeConfig,
 };
