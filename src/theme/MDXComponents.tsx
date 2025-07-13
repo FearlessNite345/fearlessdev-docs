da0d462b653043dd26f92d1dfecc0073e7fdccbb
FearlessNite345 <110247392+FearlessNite345@users.noreply.github.com>
2025-07-13 16:43:06 -0400

    Fixed button not being in integration file

---
+++ src/theme/MDXComponents.tsx
@@ -0,0 +1,8 @@
+// Importing the original mapper + our components according to the Docusaurus doc
+import MDXComponents from "@theme-original/MDXComponents";
+import Button from "@site/src/components/Button";
+export default {
+  // Reusing the default mapping
+  ...MDXComponents,
+  Button,
+};
